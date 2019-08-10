f9e3cc06d3865100440e8d1114a09aa943f68577
Kaspars <hi@kaspars.net>
2019-08-10 10:44:09 +0300

    Ensure consistent whitebalance

--- timelapse-capture.sh
+++ timelapse-capture.sh
@@ -4,5 +4,5 @@ FILENAME="photo-$(date "+%Y%m%d-%H%M%S").jpg"
 TIME=$(date +%H%M)
 
 if [ "$TIME" -ge 600 -a "$TIME" -le 2100 ]; then
-	raspistill -o "/home/pi/remote/timelapse/$FILENAME"
+	raspistill --awb off -o "/home/pi/remote/timelapse/$FILENAME"
 fi
