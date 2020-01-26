6544f02f246c12673f99045ada98bf9b2588b6a6
Kaspars <hi@kaspars.net>
2020-01-26 14:29:34 +0200

    Close camera while unused

---
+++ streamer/streamer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import io
+import picamera
+import logging
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class RequestHandler(BaseHTTPRequestHandler):
+  def _set_mjpeg_headers(self):
+    self.send_response(200)
+    self.send_header('Age', 0)
+    self.send_header('Cache-Control', 'no-cache, private')
+    self.send_header('Pragma', 'no-cache')
+    self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+    self.end_headers()
+
+  def _write_frame(self, frame):
+    self.wfile.write(b'--FRAME\r\n')
+    self.send_header('Content-Type', 'image/jpeg')
+    self.send_header('Content-Length', len(frame))
+    self.end_headers()
+    self.wfile.write(frame)
+    self.wfile.write(b'\r\n')
+
+  def do_GET(self):
+    self._set_mjpeg_headers()
+    stream=io.BytesIO()
+    with picamera.PiCamera(resolution='640x480', framerate=1) as camera:
+      try:
+        for foo in camera.capture_continuous(stream,'jpeg'):
+          self._write_frame(stream.getvalue())
+          stream.truncate()
+          stream.seek(0)
+      except Exception as e:
+        logging.warning('Camera exception: %s', str(e))
+
+try:
+  server = HTTPServer(('',8000), RequestHandler)
+  server.serve_forever()
+except Exception as e:
+  logging.warning('Main exception: %s', str(e))
+finally:
+  server.socket.close()
+
